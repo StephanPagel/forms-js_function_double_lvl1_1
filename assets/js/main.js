364de922a39ab63d0f127439c8bbdf307d9b9b8a
StephanPagel <stephan.pagel84@gmail.com>
2022-04-19 13:08:30 +0200

    finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
 console.log("it works")
+
+let inputNumber = document.getElementById('input_number');
+let output = document.getElementById('form_output');
+
+
+
+function double() {
+    console.log("function works");
+    console.log(form_output);
+    output.innerHTML = inputNumber.value * 2;
+}
